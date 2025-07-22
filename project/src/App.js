45c03514b5b1454c1eb62146f934ee35f52f3a5a
Kwamay <kwamayadu@gmail.com>
2025-07-22 12:56:40 +0000

    Add card file to project

--- project/src/App.js
+++ project/src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
+import Intro from "./components/Intro";
 import Navbar from "./components/Navbar";
+import Analytics from "./components/Analytics";
+import NewsLetter from "./components/NewsLetter";
+import Cards from "./components/Cards";
 
 function App() {
   return (
     <div >
       <Navbar />
+      <Intro />
+      <Analytics />
+      <NewsLetter />
+      <Cards />
     </div>
   );
 }
